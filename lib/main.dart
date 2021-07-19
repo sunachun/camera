192451d4d3e65e3683ebcd29a85e599b2b205881
suna0 <suna0.o4o4gym@ezweb.ne.jp>
2021-07-19 11:35:19 +0900

    ファイルシステムへの画像の保存2

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:camera/providers/great_places.dart';
+import 'package:camera/screens/add_place_screen.dart';
+import 'package:camera/screens/places_list_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider.value(
+      value: GreatPlaces(),
+      child: MaterialApp(
+          title: 'Great Places',
+          theme: ThemeData(
+            primarySwatch: Colors.indigo,
+            accentColor: Colors.amber,
+          ),
+          home: PlacesListScreen(),
+          routes: {
+            AddPlaceScreen.routeName: (ctx) => AddPlaceScreen(),
+          }),
+    );
+  }
+}
