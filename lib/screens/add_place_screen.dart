c581a958e55548b6d07e6f1f0f8a61daff978eea
suna0 <suna0.o4o4gym@ezweb.ne.jp>
2021-07-23 10:39:48 +0900

    ロケーションの追加

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:camera/providers/great_places.dart';
 import 'package:camera/widgets/image_input.dart';
+import 'package:camera/widgets/location_input.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -53,6 +54,10 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                       height: 10,
                     ),
                     ImageInput(_selectImage),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    LocationInput(),
                   ],
                 ),
               ),
